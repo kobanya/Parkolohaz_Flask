ee143e54131feee05b83403ef3e89db98ee8699a
bela <nagy.bela.budapest@gmail.com>
2023-10-05 15:01:44 +0200

    Kihajtó autók törlése, szinkronizálási hiba

--- main.py
+++ main.py
@@ -13,19 +13,16 @@ parkolohelyek_file = 'parkolohelyek.json'
 
 # Betöltjük a parkolóhelyeket a JSON fájlból
 with open(parkolohelyek_file, 'r') as file:
-    parkolohelyek_data = json.load(file)
+    parkolohelyek = json.load(file)
 
 # Lista az aktuálisan foglalt parkolóhelyekről
 aktuális_parkolohelyek = []
 
-# Frissítjük a parkolóhelyek állapotát a JSON fájlban
-def frissit_parkolohelyek_json():
-    with open(parkolohelyek_file, 'w') as file:
-        json.dump(parkolohelyek_data, file, indent=4)
-
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# ...
 
 @app.route('/ellenorzes', methods=['POST'])
 def api_ellenorzes():
@@ -34,31 +31,41 @@ def api_ellenorzes():
         rendszam = data.get('rendszam', '')
 
         # Módosítás: Ellenőrizzük, hogy a rendszám már foglalt-e
-        for hely in parkolohelyek_data['parkolohelyek']:
+        for hely in parkolohelyek['parkolohelyek']:
             if hely['rendszam'] == rendszam:
                 parkolohely_szam = hely['szam']
                 fizetendo_dij = szamol_dij(rendszam)  # Számold ki a fizetendő díjat
                 naplozas(rendszam, parkolohely_szam, "Kihajtás")
-                # Módosítás: Frissítjük a parkolóhely állapotát
+                # Módosítás: Töröljük a rendszámot a foglalt helyek közül
                 hely['foglalt'] = False
-                hely['rendszam'] = ""
-                frissit_parkolohelyek_json()
+                hely['rendszam'] = ''
+                # Mentsük el a frissített parkolóhelyeket a JSON fájlba
+                with open(parkolohelyek_file, 'w') as json_file:
+                    json.dump(parkolohelyek, json_file, indent=4)
                 return jsonify({"status": "Kihajtás", "dij": fizetendo_dij})
 
-        # Módosítás: Az üres parkolóhely helyett rendszámot kell visszaadni
-        parkolohely_szam = foglal_parkolohely(rendszam)
-        naplozas(rendszam, parkolohely_szam, "Behajtás")
-        return jsonify({"parkolohely_szam": parkolohely_szam, "status": "Behajtás"})
+        for hely in parkolohelyek['parkolohelyek']:
+            if not hely['foglalt']:
+                # Módosítás: Az üres parkolóhely helyett rendszámot kell visszaadni
+                hely['foglalt'] = True
+                hely['rendszam'] = rendszam
+                naplozas(rendszam, hely['szam'], "Behajtás")
+                # Mentsük el a frissített parkolóhelyeket a JSON fájlba
+                with open(parkolohelyek_file, 'w') as json_file:
+                    json.dump(parkolohelyek, json_file, indent=4)
+                return jsonify({"parkolohely_szam": hely['szam'], "status": "Behajtás"})
+
+        return jsonify({"status": "Teltház"})
     except Exception as e:
         return jsonify({"error": str(e)})
 
+
 # Módosítás: Hozzunk létre egy új funkciót a parkolóhely foglalásához
 def foglal_parkolohely(rendszam):
-    for hely in parkolohelyek_data['parkolohelyek']:
+    for hely in parkolohelyek['parkolohelyek']:
         if not hely['foglalt']:
             hely['foglalt'] = True
             hely['rendszam'] = rendszam
-            frissit_parkolohelyek_json()
             aktuális_parkolohelyek.append(rendszam)
             return hely['szam']
     return None
@@ -81,7 +88,7 @@ def szamol_dij(azonosito):
     # Ha van előző belépés, akkor számoljuk ki a parkolás időtartamát másodpercekben
     if elozo_belepes_idopont:
         parkolas_idotartama = (most - elozo_belepes_idopont).total_seconds()
-        fizetendo_dij = parkolas_idotartama * 10  # Minden másodperc után 10 Ft
+        fizetendo_dij = parkolas_idotartama * 1  # Minden másodperc után 1 Ft (módosítottuk)
     else:
         fizetendo_dij = 0  # Ha nincs előző belépés, nincs díj
 
@@ -92,6 +99,11 @@ def naplozas(rendszam, parkolohely, status):
     with open(naplo_file, mode='a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow([rendszam, parkolohely, idopont, status])
+
+# Új végpont a parkolóhelyek lekérdezéséhez
+@app.route('/parkolohelyek', methods=['GET'])
+def api_parkolohelyek():
+    return jsonify(parkolohelyek)
 
 if __name__ == '__main__':
     app.run(debug=True)
